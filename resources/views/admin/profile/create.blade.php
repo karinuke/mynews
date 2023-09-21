68cbfefa858650346d544518c121b1d48717fc59
karin <karin.arata1010@gmail.com>
2023-09-21 04:52:00 +0000

    kadai11テキスト内容

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,8 +1,13 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>プロフィール</title>
     </head>
     <body>
-    
+        <h1>myprofile作成画面</h1>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,12 @@ Route::controller(ProfileController::class)-> prefix('admin')-> group(function()
     Route::get('profile/create','add');
     Route::get('profile/edit','edit');
 }) ;
+
+//「http://XXXXXX.jp/XXX というアクセスが来たときに、 AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください
+use App\Http\AAAController;
+Route::controller(AAAController::class) -> group(function(){
+    Route::get('XXX','bbb');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
