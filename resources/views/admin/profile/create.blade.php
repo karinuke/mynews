5686303e03533a6a547bf6229e3f7921ff0b4784
karin <karin.arata1010@gmail.com>
2023-09-21 07:04:57 +0000

    php11課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,3 +11,21 @@
         <h1>myprofile作成画面</h1>
     </body>
 </html>
+
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>myprofile新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
