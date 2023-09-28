ad2d2bc5c12c0830a460769c2efc2b1ac6f5381e
karin <karin.arata1010@gmail.com>
2023-09-28 12:05:52 +0000

    メンタリング編集

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Profile; //書き忘れ
+
 class ProfileController extends Controller
 {
     //
